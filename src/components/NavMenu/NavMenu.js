c0e421a228e49c262128660c94e87843cf2cbaca
nickpavini <nickpavini@gmail.com>
2021-11-16 05:43:20 -0800

    style navmenu for mobile mode

--- src/components/NavMenu/NavMenu.js
+++ src/components/NavMenu/NavMenu.js
@@ -1,20 +1,31 @@
 import { Link } from 'react-scroll'
+import Fade from 'react-reveal/Fade';
+import Hamburger from 'hamburger-react'
+import React, { useState } from "react";
 import './NavMenu.css';
 
 function NavMenu() {
+  const [isOpen, setOpen] = useState(false);
+
   return (
-    <div className="nav_menu">
-        <a className="resume_button" href={process.env.PUBLIC_URL + '/NicholasPaviniResume.pdf'}>Resume</a>
-        <div className="nav_link">
-            <Link to='experience' spy={true} smooth={true} duration={500}>Experience</Link>
-        </div>
-        <div className="nav_link">
-            <Link to='projects' spy={true} smooth={true} duration={500}>Projects</Link>
-        </div>
-        <div className="nav_link">
-            <Link to='about' spy={true} smooth={true} duration={500}>About</Link>
-        </div>
-    </div>
+    <Fade>
+      <div className="nav_menu">
+          <Hamburger toggled={isOpen} toggle={setOpen} />
+          {/*isOpen ? "true" : "false"*/}
+          <div className={"nav_menu_items display_" + isOpen}>
+            <a className="resume_button" href={process.env.PUBLIC_URL + '/NicholasPaviniResume.pdf'}>Resume</a>
+            <div className="nav_link">
+                <Link to='experience' spy={true} smooth={true} duration={500}>Experience</Link>
+            </div>
+            <div className="nav_link">
+                <Link to='projects' spy={true} smooth={true} duration={500}>Projects</Link>
+            </div>
+            <div className="nav_link">
+                <Link to='about' spy={true} smooth={true} duration={500}>About</Link>
+            </div>
+          </div>
+      </div>
+    </Fade>
   );
 }
 
